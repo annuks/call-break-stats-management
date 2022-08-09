5043af8f4f21656d92ab556c7a8ce3b475d2f80e
Annu Kumar Singh <annukumarsingh.aks@gmail.com>
2022-08-09 19:31:56 +0530

    JS function Added

---
+++ assets/js/newGame.js
@@ -0,0 +1,10 @@
+
+function addPlayer(){
+let addnew = document.getElementById("add_player_container");
+regsiter.style.height = "700px"
+};
+
+function closeAddPlayer(){
+    let addnew = document.getElementById("add_player_container");
+    regsiter.style.height = "0px"
+    }
